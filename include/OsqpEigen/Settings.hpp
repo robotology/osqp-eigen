ddc1a3d63b195505c0768172f8a00a06969a89da
Karl Ljungkvist <k.ljungkvist@gmail.com>
2020-12-03 13:21:10 +0100

    use spaces instead of tabs

--- include/OsqpEigen/Settings.hpp
+++ include/OsqpEigen/Settings.hpp
@@ -95,7 +95,7 @@ namespace OsqpEigen
          * Set the max number of iterations.
          * @param maxIteration max number of iteration
          */
-		void setMaxIteration(const int maxIteration);
+        void setMaxIteration(const int maxIteration);
 
         /**
          * Set the absolute convergence tolerance.
