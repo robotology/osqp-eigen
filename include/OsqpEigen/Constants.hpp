d5bc460dfebf06450dc7393747cf7007e8bca4c5
Silvio Traversaro <silvio@traversaro.it>
2025-04-06 14:47:52 +0200

    Fix definition of OsqpEigen::Status::TimeLimitReached in osqp >= 1

--- include/OsqpEigen/Constants.hpp
+++ include/OsqpEigen/Constants.hpp
@@ -30,7 +30,7 @@ enum class Status : int
     PrimalInfeasible = OSQP_PRIMAL_INFEASIBLE,
     DualInfeasible = OSQP_DUAL_INFEASIBLE,
     Sigint = OSQP_SIGINT,
-#ifdef PROFILING
+#if defined(PROFILING) || defined(OSQP_EIGEN_OSQP_IS_V1)
     TimeLimitReached = OSQP_TIME_LIMIT_REACHED,
 #endif // ifdef PROFILING
     NonCvx = OSQP_NON_CVX,
