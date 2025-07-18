3ca529b6873439d6450f4e9c7a441a51e0868fee
Ethan J. Musser <emusser@carnegierobotics.com>
2025-07-18 17:24:56 -0400

    Link OSQP shared/static based on flag

--- cmake/OsqpEigenDependencies.cmake
+++ cmake/OsqpEigenDependencies.cmake
@@ -12,14 +12,20 @@ include(OsqpEigenFindOptionalDependencies)
 find_package(Eigen3 3.2.92 REQUIRED)
 find_package(osqp REQUIRED)
 
+if(OSQP_EIGEN_LINK_OSQP_SHARED)
+  set(OSQP_LIB osqp::osqp)
+else()
+  set(OSQP_LIB osqp::osqpstatic)
+endif()
+
 # OSQP_IS_V1 (and OSQP_EIGEN_OSQP_IS_V1) is defined for v1.0.0.beta1 and v1.0.0 (and later)
 if(NOT DEFINED OSQP_IS_V1)
-  try_compile(OSQP_IS_V1 ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_LIST_DIR}/try-osqp-v1.cpp LINK_LIBRARIES osqp::osqp)
+  try_compile(OSQP_IS_V1 ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_LIST_DIR}/try-osqp-v1.cpp LINK_LIBRARIES ${OSQP_LIB})
 endif()
 
 # OSQP_IS_V1 (and OSQP_EIGEN_OSQP_IS_V1) is defined only for v1.0.0 (and later)
 if(NOT DEFINED OSQP_IS_V1_FINAL)
-  try_compile(OSQP_IS_V1_FINAL ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_LIST_DIR}/try-osqp-v1-final.cpp LINK_LIBRARIES osqp::osqp)
+  try_compile(OSQP_IS_V1_FINAL ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_LIST_DIR}/try-osqp-v1-final.cpp LINK_LIBRARIES ${OSQP_LIB})
 endif()
 
 
