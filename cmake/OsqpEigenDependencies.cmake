4a4432542776188ef7ba094b65f4e05bcb75b8a2
Silvio Traversaro <silvio@traversaro.it>
2025-08-25 18:23:38 +0200

    Remove leftover references to LGPL license

--- cmake/OsqpEigenDependencies.cmake
+++ cmake/OsqpEigenDependencies.cmake
@@ -1,6 +1,4 @@
 # Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT). All rights reserved.
-# This software may be modified and distributed under the terms of the
-# GNU Lesser General Public License v2.1 or any later version.
 #
 # This software may be modified and distributed under the terms of the
 # BSD-3-Clause license. See the accompanying LICENSE file for details.
